41e85fa68e95163e158564c21d3df6eced69f4fd
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-10-08 12:42:16 +0300

    2.0

--- Java Test/Java Main/src/Module.java
+++ Java Test/Java Main/src/Module.java
@@ -1,9 +1,10 @@
 public class Module {
+
     public static void main(String[] args) {
         test();
     }
 
-    public static void test() {
+    private static void test() {
         System.out.println("Hello, there!");
     }
 }

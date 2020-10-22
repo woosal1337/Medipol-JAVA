7477383e6aabb2d5446f024dc5a65cfafe4545a7
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-10-22 17:58:37 +0300

    LAB

--- Java Test/Java Main/src/Module.java
+++ Java Test/Java Main/src/Module.java
@@ -1,10 +1,6 @@
 public class Module {
 
     public static void main(String[] args) {
-        test();
-    }
-
-    private static void test() {
-        System.out.println("Hello, there!");
+        System.out.println("I <3 JetBrains");
     }
 }

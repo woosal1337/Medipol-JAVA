0c8dc2df8c86080fe226e3eea76a77402b849534
Vusal Ismayilov <woosal@protonmail.com>
2020-10-27 14:42:00 +0300

    1.0

---
+++ Homework1/src/com/homework/Fourth.java
@@ -0,0 +1,7 @@
+package com.homework;
+
+public class Fourth {
+    public static void main(String[] args) {
+        System.out.println("Hello, World, 4th time here !");
+    }
+}
---
+++ Homework1/src/com/homework/Third.java
@@ -0,0 +1,7 @@
+package com.homework;
+
+public class Third {
+    public static void main(String[] args) {
+        System.out.println("Hello, World, 3!");
+    }
+}

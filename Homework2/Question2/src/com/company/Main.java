363094a5dda5edc00f20013784762089610ec8f5
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-12-07 16:27:20 +0300

    2.0

--- Homework2/Question2/src/com/company/Main.java
+++ Homework2/Question2/src/com/company/Main.java
@@ -1,6 +1,17 @@
+///////////////////////////////////////////////////////
+//
+//    Name: Vusal Ismayilov
+// 
+//    ID: 64190012
+// 
+//    Faculty: Computer Engineering
+//
+///////////////////////////////////////////////////////
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 
 public class Main {
     static Scanner scanner;
@@ -72,6 +83,95 @@ public class Main {
     public static void printMatrix(int[][] matrix) {
         int rows = matrix.length;
         int columns = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.printf("%6d ", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] multiply(int[][] a, int[][] b) {
+        if (a[0].length == b.length) {
+            System.out.println("The operation cannot be performed");
+            int[][] result = new int[a.length][a[0].length];
+            return result;
+        } else {
+            int[][] result = new int[a.length][a[0].length];
+
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < b[0].length; j++) {
+                    for (int z = 0; z < b.length; z++)
+                        result[i][j] += a[i][z] * b[z][j];
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public static int[][] transpose(int[][] a) {
+        int[][] result = new int[a.length][a[0].length];
+        if (a.length != a[0].length) {
+            System.out.println("Error!");
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[0].length; j++) {
+                    result[i][j] = a[j][i];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static int minOfElements(int[][] a) {
+        int min = a[0][0];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if (min < a[i][j]) {
+                    min = a[i][j];
+                }
+            }
+        }
+        return min;
+    }
+
+    public static int sumOfElements(int[][] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                sum += a[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static int[][] add(int[][] a, int[][] b) {
+        int[][] finalResult = new int[a.length][a[0].length];
+        if (a.length != b.length || a[0].length != b[0].length) {
+            System.out.println("Error!");
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[0].length; j++) {
+                    finalResult[i][j] = a[i][j] + b[i][j];
+                }
+            }
+        }
+        return finalResult;
+    }
+
+    public static int[][] subtract(int[][] a, int[][] b) {
+        int[][] finalResult = new int[a.length][a[0].length];
+        if (a.length != b.length || a[0].length != b[0].length) {
+            System.out.println("Error!");
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[0].length; j++) {
+                    finalResult[i][j] = a[i][j] - b[i][j];
+                }
+            }
+        }
+        return finalResult;
     }
 
 

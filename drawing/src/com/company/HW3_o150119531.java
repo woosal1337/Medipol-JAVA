619c9e86a19a9a1124a6edebcad8653d9e565854
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-12-25 13:14:14 +0300

    LAB6.0

--- drawing/src/com/company/HW3_o150119531.java
+++ drawing/src/com/company/HW3_o150119531.java
@@ -1,14 +1,22 @@
+// Name : Mahmoud suhel mahmoud alkifiri
+// Student Number: o150119531
+//the purpose of this program is to draw one of the 3 shapes(Line, Parabola, or circle) and it
+//will depend on the certain parameters and inputs of the shapes using the mathematical and geometrical formulas that each have
 package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class HW3_o150119531 {
+    // Declaring the scanner here to use in the rest of the functions below
     static Scanner theScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        // Taking the first one time user input here
         System.out.println("Which shape would you like to draw?\n1. Line \n2. Parabola \n3. Circle\n4. Exit");
         int userChoiceInput = theScanner.nextInt();
 
+        // Based on the given input running the according function below
         while (0 < userChoiceInput && userChoiceInput < 5) {
             if (userChoiceInput == 1) {
                 Line();
@@ -22,13 +30,18 @@ public class Main {
         }
     }
 
+    // This is where the drawing the line function is called
     public static void Line() {
 
+        // Getting the user input here again to complete the line function
         System.out.println("Line formula is y = ax + b");
         System.out.print("Please enter coefficients a and b: ");
         double a = theScanner.nextDouble();
         double b = theScanner.nextDouble();
 
+        // Based on the function above with the given user input
+        // Both for X and Y axis, drawing the graph and in the needed palces graphing
+        // the stars
         for (int i = 10; i >= -10; i--) {
             for (int j = -10; j <= 10; j++) {
                 double y = a * j + b;
@@ -47,6 +60,10 @@ public class Main {
             }
             System.out.println();
         }
+
+        // Drawing the graph is done, so function here will call the user to give
+        // another input to run the function again.
+        // A kind of recursive function happens here.
         System.out.println("Which shape would you like to draw?\n1. Line \n2. Parabola \n3. Circle\n4. Exit");
         int userChoiceInput = theScanner.nextInt();
 
@@ -63,14 +80,17 @@ public class Main {
         }
     }
 
+    // Drawing Parabola related function happens below
     public static void Parabola() {
 
+        // Getting the user input as well to prepare the parabola function
         System.out.println("Parabola formula is y = ax^2 + bx + c");
         System.out.print("Please enter parabola's coefficients a, b and c: ");
         double a = theScanner.nextDouble();
         double b = theScanner.nextDouble();
         double c = theScanner.nextDouble();
 
+        // Again looping through both X and Y axis to draw the final parabola below:
         for (int i = 10; i >= -10; i--) {
             for (int j = -10; j <= 10; j++) {
                 double y = a * j * j + b * j + c;
@@ -89,6 +109,10 @@ public class Main {
             }
             System.out.println();
         }
+
+        // Drawing the graph is done, so function here will call the user to give
+        // another input to run the function again.
+        // A kind of recursive function happens here.
         System.out.println("Which shape would you like to draw?\n1. Line \n2. Parabola \n3. Circle\n4. Exit");
         int userChoiceInput = theScanner.nextInt();
 
@@ -105,15 +129,20 @@ public class Main {
         }
     }
 
+    // Drawing Circle related function happens below
     public static void Circle() {
 
+        // Here as well getting the user input for the a, b, and the radius to comlete
+        // the circle function and iterate
+        // below through the loops to draw the circle
         System.out.println("Circle formula is (x-a)^2 + (y-b)^2 = r^2 ");
         System.out.print("Please enter circle's coefficients a, b and radius: ");
         double a = theScanner.nextDouble();
         double b = theScanner.nextDouble();
         double r = theScanner.nextDouble();
 
-
+        // Drawing the graph and iterating through the given user input, and the loops
+        // to draw the circle happens below:
         for (int i = 10; i >= -10; i--) {
             for (int j = -10; j <= 10; j++) {
                 if ((j - a) * (j - a) + (i - b) * (i - b) == r * r) {
@@ -131,6 +160,9 @@ public class Main {
             System.out.println();
         }
 
+        // Drawing the graph is done, so function here will call the user to give
+        // another input to run the function again.
+        // A kind of recursive function happens here.
         System.out.println("Which shape would you like to draw?\n1. Line \n2. Parabola \n3. Circle\n4. Exit");
         int userChoiceInput = theScanner.nextInt();
 
@@ -144,6 +176,8 @@ public class Main {
             } else {
                 System.exit(0);
             }
+        // A special function for the 4th case (Exit) was not created as System.exit(0) 
+        // exits the code instantly does not matter where it was used.
         }
     }
 }
